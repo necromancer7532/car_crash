0542763e2ed48525c7d51347f034c22bf55ddd06
Vishnu Remesh <vishnu.remesh@zs.com>
2022-04-03 00:09:03 +0530

    made changes to code logic

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import datetime
@@ -10,15 +12,17 @@ log4jLogger = spark._jvm.org.apache.log4j
 logger = log4jLogger.LogManager.getLogger(__name__)
 logger.setLevel(log4jLogger.Level.INFO)
 
+if len(sys.argv) < 2:
+    logger.error("PLEASE PASS THE FILE LOCATION AS ARGUMENT")
+    exit(0)
+file_path = sys.argv[1]
+################################################# Analysis 1 #################################################
 file_read_obj = FileRead(spark)
-df_file = file_read_obj.read_data_from_file("./input_files/Primary_Person_use.csv","csv")
+df_file = file_read_obj.read_data_from_file(file_path+"/Primary_Person_use.csv", "csv")
 
 accident_count_obj = AccidentCount(spark)
 
 count_accident = accident_count_obj.count_accidents("MALE", "KILLED", df_file)
 
-print(count_accident)
-logger.info("Number of accidents where a male died : {}".format(count_accident))
-
-
+logger.info("Analysis 1 : Number of accidents where a male died : {}".format(count_accident))
 
