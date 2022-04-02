15eacf81cec63ced6b905d1038928682bc7e9dd9
Vishnu Remesh <vishnu.remesh@zs.com>
2022-04-02 23:34:31 +0530

    made changes to log4j logger

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,10 @@ from accident_count import AccidentCount
 
 spark = SparkSession.builder.getOrCreate()
 
+log4jLogger = spark._jvm.org.apache.log4j
+logger = log4jLogger.LogManager.getLogger(__name__)
+logger.setLevel(log4jLogger.Level.INFO)
+
 file_read_obj = FileRead(spark)
 df_file = file_read_obj.read_data_from_file("../input_files/Primary_Person_use.csv","csv")
 
