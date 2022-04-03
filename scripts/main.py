4d5b213e747a07189efaec6ccdb5558954e1adf8
Vishnu Remesh <vishnu.remesh@zs.com>
2022-04-03 13:01:51 +0530

    made changes to code for q4

--- scripts/main.py
+++ scripts/main.py
@@ -78,5 +78,6 @@ elif question_no == '4':
     df_result = vehicle_booked_obj.vehicle_model_count(df_dict)
 
     logger.info("Analysis 3 : Top 5th to 15th VEH_MAKE_IDs that contribute to the largest number of injuries "
-                "including death : {}".format(df_result.show()))
+                "including death :")
+    df_result.show()
     spark.stop()
