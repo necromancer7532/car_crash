d78c342db3717f3efec0507571459f960cb90edd
Vishnu Remesh <vishnu.remesh@zs.com>
2022-04-03 09:43:29 +0530

    made changes to keys in json

--- scripts/file_read_util.py
+++ scripts/file_read_util.py
@@ -6,6 +6,7 @@ class FileRead:
         dataframes_dict = {}
         for item in file_names:
             file_name = file_path + '/' + item + '.' + file_type
+            print(file_name)
             df_read = self.spark_context.read.load(file_name, file_type, header="true", inferSchema="true",
                                                    multiLine="true")
             dataframes_dict[file_name] = df_read
